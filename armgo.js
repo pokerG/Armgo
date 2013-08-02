90a665bc10db5f78fd2720e667d4ee42b8964106
pokerG <pokerfacehlg@gmail.com>
2013-08-02 09:55:20 +0800

    two

--- armgo.js
+++ armgo.js
@@ -1,14 +1,7 @@
 // Preferences
 var Fz = 20;
 
-// Global Variables
-var c = eid("canvas");
-var cxt = c.getContext("2d");
-var ctime;				// main game timer
-var Mission;			// Mission Canvas Object
-var ns = eid("ns");		// debug message
-var state = [];			// the state of stage
-
+// Const Variables
 var ms = {
 	down : false
 };
@@ -35,14 +28,24 @@ var STATE = {
 	y : 100
 }
 
+// Global Variables
+var c = eid("canvas");
+var cxt = c.getContext("2d");
+var ctime;				// arm Go!
+var Mission;			// Mission Canvas Object
+var ns = eid("ns");		// debug message
+var state = [];			// the state of stage
+
+// object
 var arm = new ARM();
 var cope = new COPE(1);
 var missionList = new MISSIONLIST();
+var runs = new RUNS();
 
 function COPE(v){
 	this.x = 50;
 	this.y = 50;
-	this.width = 40;
+	this.width = 35;
 	this.height = this.width;
 	this.value = v;
 }
@@ -51,45 +54,59 @@ function ARM(){
 	this.leftz = STATE.x - CELL.x - 12;
 	this.topz = STATE.y;
 	this.bottonz = STATE.y + CELL.y * 12;
-	this.x = this.leftz;
-	this.y = this.topz;
-	this.r = 1;
 	this.speed = 6;
 	this.color = '#00ff00';
-	this.hand = 0;
-	this.right = function(){
+	this.init = function(){
+		this.x = this.leftz;
+		this.y = this.topz;
+		this.r = 1;
+		this.hand = 0;
+	}
+	this.init();
+	this.done = function(v,i){
+		if ( runs.tasks[v][i+1] != 0 ) runs.run(v,i+1);
+	}
+	this.right = function(v,ii){
 		var i = 6;
 		while ( i>0 && state[this.r-1][i-1] != 0 ) i--;
-		this.goRight(STATE.x+CELL.x*i);
+		this.goRight(STATE.x+CELL.x*i,v,ii);
 	}
-	this.left = function(){
+	this.left = function(v,i){
 		this.x -= this.speed ;
 		if ( this.x > this.leftz ){
-			setTimeout('arm.left()',Fz);
+			setTimeout('arm.left('+v+','+i +')',Fz);
+			//sleep(Fz);this.left();
+		}else {
+			this.done(v,i);
 		}
 	}
-	this.up = function(){
+	this.up = function(v,i){
 		if ( this.r == 1 ) return ;
 		this.y -= this.speed ;
 		if (this.y > (STATE.y+CELL.y*(this.r-2)*2) ) {
-			setTimeout('arm.up()',Fz);
+			setTimeout('arm.up('+v+','+i+')',Fz);
+			//sleep(Fz);this.up();
 		}else {
 			this.r --;
+			this.done(v,i);
 		}
 	}
-	this.down = function(){
+	this.down = function(v,i){
 		if ( this.r == 6 ) return ;
 		this.y += this.speed ;
 		if ( this.y < ( STATE.y+CELL.y*(this.r*2))) {
-			setTimeout('arm.down()',Fz);
+			setTimeout('arm.down('+v+','+i+')',Fz);
+			//sleep(Fz);this.down();
 		}else {
 			this.r ++ ;
+			this.done(v,i);
 		}
 	}
-	this.goRight = function(x){
+	this.goRight = function(x,v,ii){
 		this.x 	+= this.speed ;
 		if ( this.x + (this.hand > 0)*CELL.x  < x ){
-			setTimeout('arm.goRight('+x+')',Fz);
+			setTimeout('arm.goRight('+x+','+v+','+ii+')',Fz);
+			//sleep(Fz);this.goRight(x);
 		}if ( this.x + (this.hand > 0)*CELL.x  == x ){
 			
 			var i = 6;
@@ -107,34 +124,96 @@ function ARM(){
 				state[this.r-1][i] = 0;
 			}
 			
-			this.left();
+			this.left(v,ii);
 			setTimeout('checkAns()',200);
 		}
 	}
 }
 
 function MISSIONLIST(){
-	this.tot = 1;
-	this.startRow = 100;
-	this.endRow = c.width;
-	this.startCol = 100;
-	this.endCol = c.height;
+	this.tot = Goal.length-1;
+	this.x = 90;
+	this.y = 150;
+	this.r = 80;
+	this.c = 80;
 	this.show = function(){
-		alert(this.startRow);
+		clearTimeout(ctime);
 		cxt.fillStyle = '#000';
 		cxt.fillRect(0,0,c.width,c.height);
-		/*
-		for ( i=this.startCol; i<this.endCol; i+=cope.height*2){
-			for ( j=this.startRow; j<this.endRow; j+=cope.width*2){
-				drawCope(j,i,1);
-			}
+		cxt.fillStyle = '#fff';
+		cxt.font = "40px Arial";
+		cxt.fillText("Mission:",this.x,this.y-50);
+		for ( i=1; i<=this.tot; i++){
+			var x = this.x + ((i-1)%5)*this.r*2;
+			var y = this.y + Math.floor((i-1)/5)*this.c*2;
+			drawCope(x,y,i+3,this.r,this.c);
 		}
-		*/
-		for ( i=100; i<300; i+=cope.height*2){
-			for ( j=100; j<500; j+=cope.width*2){
-				drawCope(j,i,1);
-			}
+		c.onclick = function(e){
+			missionList.selectMission(e)
 		}
+	}
+	this.selectMission = function(e){
+		initVal(getClickId(e,missionList.x,missionList.y,missionList.r*2,missionList.c*2,5));
+	}
+}
+
+function RUNS(){
+	this.init = function(){
+		this.tasks=[[1,5,1,0,0,0,0,0],[3,1,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0]];
+		this.ifs=[[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0]];
+	}
+	this.init();
+	this.x = STATE.x + CELL.x*8;
+	this.y = STATE.y - CELL.y;
+	this.r = 300;
+	this.c = 300;
+	this.draw = function(){
+		for ( i=0; i<4; i++){
+			for ( j=1; j<9; j++){
+				drawCope(this.x+cope.width*j,this.y+cope.height*i*1.7,this.tasks[i][j-1],cope.width,cope.height);
+				if ( i==3 && j==5 ) break;
+			}
+			drawCope(this.x,this.y+cope.height*i*1.7,i+4,cope.width,cope.height);
+		}
+	}
+	this.run = function(v,i){
+		ns.innerHTML = v + ',' + i ;
+			switch( this.tasks[v][i] ){
+				case 1 :
+					arm.right(v,i);
+					break;
+				case 2 :
+					arm.up(v,i);
+					break;
+				case 3 :
+					arm.down(v,i);
+					break;
+				case 4 :
+					this.run(0,0);
+					break;
+				case 5 :
+					this.run(1,0);
+					break;
+				case 6 :
+					this.run(2,0);
+					break;
+				case 7 :
+					this.run(3,0);
+					break;
+			}
+	}
+}
+
+function getClickId(e,x,y,r,c,m){	// ID of (e.x,e.y) in Map: start(x,y),per(r,c), there are m in one line 
+	var i = Math.floor(( e.clientX - eid("canvas").offsetLeft - x ) / r ) + 1;
+	var j = Math.floor(( e.clientY - eid("canvas").offsetTop - y ) / c )+ 1;
+	eid("canvas").style.cursor = 'default';
+	var ttt = (j-1)*m+i ;
+	if ( ttt > 0 && ttt <= missionList.tot ){
+		eid("canvas").onclick = function() { null; };
+		return ttt;
+	}else{
+		return getClickId(e,missionList.x,missionList.y,missionList.r*2,missionList.c*2,5);
 	}
 }
 
@@ -154,7 +233,8 @@ function initVal(v){
 			state[i][j] = startM[Mission][i][j];
 		}
 	}
-	flashMap(v);
+	arm.init();
+	ctime = setInterval('flashMap()',Fz);
 }
 
 function flashMap(){
@@ -164,7 +244,7 @@ function flashMap(){
 	drawState();
 	drawArm();
 	drawBAG();
-	setTimeout('flashMap()',Fz);
+	runs.draw();
 }
 
 function drawHello(){
@@ -174,16 +254,11 @@ function drawHello(){
 	gradient.addColorStop("0","#fff");
 	gradient.addColorStop("1.0","#777");
 	cxt.fillStyle = gradient;
-	cxt.font = "50px Arial Black";
-	cxt.fillText("ArmGo",c.width/2-100,c.height/2);
+	cxt.font = "80px Arial Black";
+	cxt.fillText("ArmGo",c.width/2-150,c.height/2);
 	
-	var kk = eid("GoMission");
-	kk.style.left = c.width/2-100;
-	kk.style.top = c.height/2;
-	kk.addEventListener('click',missionList.show,false);
-	
-	initVal(1);
-	flashMap();
+	cxt.font = "20px Arial Black";
+	cxt.fillText("click here",c.width/2-60,c.height/2+50);
 }
 
 function drawBg(){
@@ -205,7 +280,8 @@ function drawGoal(){
 	var y = GOALM.y;
 	var g = Goal[Mission];
 	cxt.fillStyle = '#000';
-	cxt.fillText("Goal",x,y-20);
+	cxt.font = '30px Arial';
+	cxt.fillText("GOAL:",x,y-20);
 	for ( i=0; i<6; i++ ){
 		for ( j=0; j<6; j++){
 			cxt.fillStyle = color[g[i][j]];
@@ -230,7 +306,7 @@ function drawState(){
 			if ( g[i][j] != 0){
 				cxt.fillRect(j*CELL.x+x,i*CELL.y*2+y,CELL.x,CELL.y);
 			}else {
-				cxt.strokeRect(j*CELL.x+x,i*CELL.y*2+y,CELL.x,CELL.y);
+				//cxt.strokeRect(j*CELL.x+x,i*CELL.y*2+y,CELL.x,CELL.y);
 			}
 		}
 	}
@@ -287,37 +363,40 @@ function drawCell(x,y,v){
 
 function checkAns(){
 	if ( state.toString() == Goal[Mission].toString()){
+		clearTimeout(ctime);
 		alert("WIN");
+		missionList.show();
 	}
 }
 
-function Wmove(e){
-	ns.innerHTML = (e.clientX - c.offsetLeft) + "," + (e.clientY - c.offsetTop);
-	if (ms.down){
-		var copi = eid("cope1");
-		copi.style.left = e.clientX - cope.width/2;
-		copi.style.top = e.clientY - cope.height/2;
-	}
+function sleep(s){
+	flashMap();
+	var startTime = new Date().getTime();
+	while ( new Date().getTime() < startTime + s ) ;
+}
 
+function Wmove(e){
+	//ns.innerHTML = (e.clientX - c.offsetLeft) + "," + (e.clientY - c.offsetTop);
 }
 
 function Wdown(e){
 }
 	
 function Wup(e){
-	ms.down = false;
+
 }
 
 function Cdown (e) {
-	ms.down = true;
+
 }
 
-window.addEventListener('mousedown',Wdown,false);
-window.addEventListener('mouseup',Wup,false);
 window.addEventListener('mousemove',Wmove,false);
 
-eid("cope1").addEventListener('mousedown',Cdown,false);
-
 window.onload = function(){
+	//initVal(1);
 	drawHello();
+	c.onclick = function(){
+		c.onclick = function(){};	
+		missionList.show();
+	}
 }
